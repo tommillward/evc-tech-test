17c52ef70fbd77e9effb83306ca19249a8ceac09
Tom Millward <tommillward10@gmail.com>
2023-12-11 18:54:37 +0000

    wrap app in provider

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import "react-datepicker/dist/react-datepicker.css";
+import { Provider } from "react-redux";
+import store from "./store";
 
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -11,7 +13,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
